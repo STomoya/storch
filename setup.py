8e9efe45cb9c7fa309b9a04d379305a357d561f4
STomoya <blackie0110@gmail.com>
2022-04-14 17:51:37 +0900

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='storch',
-    version='0.0.24',
+    version='0.0.25',
     license='MIT',
     description='PyTorch utilities for STomoya.',
     author='Tomoya Sawada (STomoya)',
