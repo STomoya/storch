3764764daf7a798ddb3bda9973531802817b1514
STomoya <blackie0110@gmail.com>
2022-07-20 20:20:42 +0900

    [ADD] mixup

---
+++ storch/algorithms/mixup.py
@@ -0,0 +1,66 @@
+
+from __future__ import annotations
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+
+def mixup(
+    images: torch.Tensor, targets: torch.Tensor, alpha: float=0.2, p: float=1.0, sample_wise: bool=True
+) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    '''Mixup augmentation
+
+    Arguments:
+        images: torch.Tensor
+            Tensor of images to apply mixup to.
+        targets: torch.Tensor
+            Tensor of targets.
+        alpha: float (default: 1.0)
+            Parameter for sampling random numbers from the Beta distribution.
+        p: float (default: 1.0)
+            Defaults to 1.0, because mixup is originally always applied.
+            Probability to apply mixup to images.
+            If sample_wise is true, determined on every sample.
+        sample_wise: bool (default: True)
+            Make a mask for each samples in the batch.
+    '''
+    B = images.size(0)
+
+    lambdas = torch.ones(B, 1, 1, 1, device=images.device)
+    if sample_wise:
+        for i in range(B):
+            if np.random.random(1) < p:
+                lambda_ = np.random.beta(alpha, alpha)
+                lambdas[i] = lambda_
+    elif np.random.random(1) < p:
+        lambda_ = np.random.beta(alpha, alpha)
+        lambdas = lambdas * lambda_
+
+    permutation = torch.randperm(B, device=images.device)
+    mixed = images * lambdas + images[permutation] * (1 - lambdas)
+
+    return mixed, targets[permutation], lambdas.view(-1)
+
+
+def mixed_cross_entropy_loss(logits, targets_a, targets_b, lambdas):
+    '''Cross entropy loss for mixed images
+
+    Arguments:
+        logits: torch.Tensor
+            Output logits of the model.
+        targets_a: torch.Tensor
+            targets of the original batch.
+        targets_b: torch.Tensor
+            targets returned by mixup()
+        lambdas: torch.Tensor
+            lambdas returned by mixup()
+
+    Returns:
+        loss: torch.Tensor
+            The calculated loss.
+    '''
+    ce_loss_a = F.cross_entropy(logits, targets_a, reduction='none') * lambdas
+    ce_loss_b = F.cross_entropy(logits, targets_b, reduction='none') * (1 - lambdas)
+    loss = (ce_loss_a + ce_loss_b).mean()
+    return loss
