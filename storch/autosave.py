01a5e89d708b19bf7ab5a393161b340a60d27bec
STomoya <blackie0110@gmail.com>
2022-09-19 13:57:05 +0900

    [UP] docstring to google format

--- storch/autosave.py
+++ storch/autosave.py
@@ -12,7 +12,7 @@ __all__ = [
 ]
 
 class autosave:
-    '''autosave registered objects when code exits.
+    """autosave registered objects when code exits.
     This class is only a container for the objects to be saved,
     with functions to register objects.
 
@@ -20,34 +20,30 @@ class autosave:
           forces the user to take care of this, such as registering torch tensors on GPU
           or calling registering functions multiple times.
 
-    Attrs:
-        _container: dict
-            contains the objects
-    '''
+    Attributes:
+        _container (dict): contains the objects to be saved.
+    """
 
     _container = {}
 
     @classmethod
     def register_object(cls, name: str, obj: Any, overwrite: bool=False) -> None:
-        '''register an object to save
+        """register an object to save
 
-        Arguments:
-            name: str
-                name to identify the object
-            obj: Any
-                the object to register
-            overwrite: bool (default: False)
-                overwrite the object if already exists
+        Args:
+            name (str): name to identify the object.
+            obj (Any): the object to register.
+            overwrite (bool, optional): overwrite the object if already exists. Default: False.
 
-        Usage:
-            # anything that can be torch.save()-ed san be registered
-            # optionally overwrite the object using 'overwrite' argument.
-            autosave.register_object('name-of-object', torch.randn(10), overwrite=True)
+        Examples::
+            >>> # anything that can be torch.save()-ed can be registered
+            >>> # optionally overwrite the object using 'overwrite' argument.
+            >>> autosave.register_object('name-of-object', torch.randn(10), overwrite=True)
 
-            # model.state_dict() returns a reference so there is no need to overwrite
-            # and only needs to be called once
-            autosave.register_object('model', model.state_dict())
-        '''
+            >>> # model.state_dict() returns a reference so there is no need to overwrite
+            >>> # and only needs to be called once
+            >>> autosave.register_object('model', model.state_dict())
+        """
         if name not in cls._container:
             cls._container[name] = obj
         elif overwrite:
@@ -55,20 +51,22 @@ class autosave:
 
     @classmethod
     def register_function_output(cls, func: Callable) -> Callable:
-        '''register a function to save the outputs.
+        """register a function to save the outputs.
 
-        Arguments:
-            func: Callable
-                The function to save the outputs of
+        Args:
+            func (Callable): The function to save the outputs of
 
-        Usage:
-            # it can be used like a function
-            func = autosave.register_function_output(func)
-            # or a decorator
-            @autosave.register_function_output
-            def func():
-                return
-        '''
+        Returns:
+            Callable: The registered function.
+
+        Examples::
+            >>> # it can be used like a function
+            >>> func = autosave.register_function_output(func)
+            >>> # or a decorator
+            >>> @autosave.register_function_output
+            >>> def func():
+            >>>     return
+        """
         name = func.__qualname__
         @wraps(func)
         def inner(*args, **kwargs):
@@ -80,8 +78,13 @@ class autosave:
 
 
 def _save(folder='.'):
-    '''save registered objects
-    '''
+    """save registered objects.
+    This function does not need to be called manually. It is automatically called at exit
+    via atexit.
+
+    Args:
+        folder (str, optional): The directory to save the registered objects. Default: '.'.
+    """
     if len(autosave._container.keys()):
         torch.save(autosave._container, os.path.join(folder, 'autosave.torch'))
 
