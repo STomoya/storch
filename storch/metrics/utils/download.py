0fbfbea8937f2610898bfc6ae0171b9d044ce410
STomoya <blackie0110@gmail.com>
2022-09-29 16:24:25 +0900

    [ADD] utils for generative model metrics

---
+++ storch/metrics/utils/download.py
@@ -0,0 +1,29 @@
+
+import os
+import shutil
+import urllib.request
+
+from storch.path import Path
+
+
+def download_url(url: str, filename: str, folder: str='./.cache/storch/metrics') -> str:
+    """download url
+
+    Args:
+        url (str): URL to the file to download.
+        filename (str): filename to be saved as
+        folder (str, optional): the folder to save the downloaded file to. Default: './.cache/storch/metrics'.
+
+    Returns:
+        str: the path to the downloaded file.
+    """
+    folder = Path(folder)
+    ckpt_path = folder / filename
+    print(f'Downloading: "{url}" to {ckpt_path.resolve()}')
+    if not ckpt_path.exists():
+        if not folder.exists():
+            os.makedirs(folder)
+        with urllib.request.urlopen(url) as response, open(ckpt_path, 'wb') as fp:
+            shutil.copyfileobj(response, fp)
+
+    return ckpt_path
---
+++ storch/metrics/utils/resnet.py
@@ -0,0 +1,97 @@
+
+import torch
+import torch.nn as nn
+from torchvision.models.resnet import ResNet50_Weights, resnet50
+
+try:
+    from torchvision.models.resnet import ResNet50_Weights
+except ImportError:
+    ResNet50_Weights = None
+
+from storch.metrics.utils.download import download_url
+
+SWAV_IN1K_URL = 'https://dl.fbaipublicfiles.com/deepcluster/swav_800ep_pretrain.pth.tar'
+
+
+class _ResNet(nn.Module):
+    """Base class for ResNet50 feature extractors."""
+    def __init__(self,
+        feature_dims=2048,
+        weight_folder='./.cache/storch/metrics', filename=None
+    ) -> None:
+        super().__init__()
+        feature_dim2block_index = {256: 0, 512: 1, 1024: 2, 2048: 3}
+        assert feature_dims in feature_dim2block_index.keys(), f'feature_dims must be one of {list(feature_dim2block_index.keys())}'
+        self.block_index = feature_dim2block_index[feature_dims]
+        self.ckpt_path, resnet = self.load_checkpoint(weight_folder, filename)
+
+        self.conv1 = resnet.conv1
+        self.bn1 = resnet.bn1
+        self.relu = resnet.relu
+        self.maxpool = resnet.maxpool
+        self.layer1 = resnet.layer1
+        self.layer2 = resnet.layer2
+        self.layer3 = resnet.layer3
+        self.layer4 = resnet.layer4
+        self.avgpool = resnet.avgpool
+
+    def load_checkpoint(self, weight_folder, filename):
+        raise NotImplementedError()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        if self.block_index == 0:
+            x = self.avgpool(x).flatten(1)
+            return x
+
+        x = self.layer2(x)
+        if self.block_index == 1:
+            x = self.avgpool(x).flatten(1)
+            return x
+
+        x = self.layer3(x)
+        if self.block_index == 2:
+            x = self.avgpool(x).flatten(1)
+            return x
+
+        x = self.layer4(x)
+        x = self.avgpool(x).flatten(1)
+        return x
+
+
+class ResNetIN(_ResNet):
+    """Supervised ResNet50"""
+    def load_checkpoint(self, weight_folder, filename):
+        ckpt_path = None
+        if ResNet50_Weights is not None:
+            resnet = resnet50(weights=ResNet50_Weights.DEFAULT)
+        else:
+            resnet = resnet50(pretrained=True)
+        return ckpt_path, resnet
+
+
+class ResNetSwAVIN(_ResNet):
+    """Self-supervised ResNet50 (SwAV)"""
+    def __init__(self, feature_dims=2048, weight_folder='./.cache/storch/metrics', filename='swav-in1k-resnet50.torch') -> None:
+        super().__init__(feature_dims, weight_folder, filename)
+
+    def load_checkpoint(self, weight_folder, filename):
+        ckpt_path = download_url(SWAV_IN1K_URL, filename, weight_folder)
+
+        state_dict = torch.load(ckpt_path, map_location='cpu')
+        new_state_dict = {}
+        for key, value in state_dict.items():
+            # erase 'module.' prefix
+            key = key.replace('module.', '')
+            # we don't need ssl specific weights.
+            if not key.startswith('projection_head.') and not key.startswith('prototypes'):
+                new_state_dict[key] = value
+        resnet = resnet50()
+        resnet.fc = None
+        resnet.load_state_dict(new_state_dict)
+        return ckpt_path, resnet
