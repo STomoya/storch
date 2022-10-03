f0a2a084167256a8181f139647505b06610c580e
Tomoya Sawada <stomoya0110@gmail.com>
2022-10-03 11:49:02 +0900

    [UP] native amp availability

--- storch/utils/pt_version.py
+++ storch/utils/pt_version.py
@@ -6,3 +6,7 @@ import torch
 import torchvision
 
 __pytorch_version__ = parse_version(torch.__version__)
+
+
+def is_native_amp_available():
+    return __pytorch_version__ >= parse_version('1.6.0')
