e2573ca769273e80e6c9e30cf12e554b0b928ffa
Tomoya Sawada <stomoya0110@gmail.com>
2022-11-14 17:23:06 +0900

    [UP] add import

--- storch/metrics/__init__.py
+++ storch/metrics/__init__.py
@@ -1,3 +1,4 @@
+from storch.metrics.best_model import BestStateKeeper, KeeperCompose
 from storch.metrics.classification import test_classification
 from storch.metrics.generative import MetricFlags, calc_metrics
 from storch.metrics.image import psnr, ssim
