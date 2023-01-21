83c412c62311418f0457326d58acc24ddf123564
Tomoya Sawada <stomoya0110@gmail.com>
2023-01-21 21:36:57 +0900

    [UP] change var naming

--- storch/utils/version.py
+++ storch/utils/version.py
@@ -4,17 +4,17 @@ import torch
 import torchvision
 from pkg_resources import parse_version
 
-__pytorch_version__ = parse_version(torch.__version__)
-__ptvision_version__ = parse_version(torchvision.__version__)
+_PYTORCH_VERSION = parse_version(torch.__version__)
+_TORCHVISION_VERSION = parse_version(torchvision.__version__)
 
 
 def is_native_amp_available():
-    return __pytorch_version__ >= parse_version('1.6.0')
+    return _PYTORCH_VERSION >= parse_version('1.6.0')
 
 
 def is_multi_weight_api_available():
-    return __ptvision_version__ >= parse_version('0.13.0')
+    return _TORCHVISION_VERSION >= parse_version('0.13.0')
 
 
 def is_compiler_available():
-    return __pytorch_version__ >= parse_version('2.0.0')
+    return _PYTORCH_VERSION >= parse_version('2.0.0')
