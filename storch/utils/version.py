669698ba5a15cb43f952004421b3228d7d3d2ba0
Tomoya Sawada <stomoya0110@gmail.com>
2023-01-21 21:36:06 +0900

    [UP] rename file

--- storch/utils/version.py
+++ storch/utils/version.py
@@ -1,9 +1,8 @@
 """pytorch availability"""
 
-from pkg_resources import parse_version
-
 import torch
 import torchvision
+from pkg_resources import parse_version
 
 __pytorch_version__ = parse_version(torch.__version__)
 __ptvision_version__ = parse_version(torchvision.__version__)
@@ -15,3 +14,7 @@ def is_native_amp_available():
 
 def is_multi_weight_api_available():
     return __ptvision_version__ >= parse_version('0.13.0')
+
+
+def is_compiler_available():
+    return __pytorch_version__ >= parse_version('2.0.0')
