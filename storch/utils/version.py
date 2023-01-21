1a394c8a2f99d0ffbb96eb5e71099c9e58ca7a73
Tomoya Sawada <stomoya0110@gmail.com>
2023-01-21 23:29:09 +0900

    [UP] add version checkers

--- storch/utils/version.py
+++ storch/utils/version.py
@@ -11,13 +11,31 @@ _PYTORCH_VERSION = parse_version(torch.__version__)
 _TORCHVISION_VERSION = parse_version(torchvision.__version__)
 
 
+def _is_version_geq(current, minimum):
+    if isinstance(minimum, str):
+        minimum = parse_version(minimum)
+    return current >= minimum
+
+
+def is_python_version_geq(minimum):
+    return _is_version_geq(_PYTHON_VERSION, minimum)
+
+
+def is_torch_version_geq(minimum):
+    return _is_version_geq(_PYTORCH_VERSION, minimum)
+
+
+def is_torchvision_version_geq(minimum):
+    return _is_version_geq(_TORCHVISION_VERSION, minimum)
+
+
 def is_native_amp_available():
-    return _PYTORCH_VERSION >= parse_version('1.6.0')
+    return is_torch_version_geq('1.6.0')
 
 
 def is_multi_weight_api_available():
-    return _TORCHVISION_VERSION >= parse_version('0.13.0')
+    return is_torchvision_version_geq('0.13.0')
 
 
 def is_compiler_available():
-    return _PYTORCH_VERSION >= parse_version('2.0.0')
+    return is_torchvision_version_geq('2.0.0')
