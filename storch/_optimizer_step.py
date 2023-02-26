37f3185523071048fff5f289b7a5035f99ee3d4a
Tomoya Sawada <stomoya0110@gmail.com>
2023-02-26 14:42:31 +0900

    [UP] add simpler impl

--- storch/_optimizer_step.py
+++ storch/_optimizer_step.py
@@ -12,6 +12,7 @@ from torch.cuda.amp import GradScaler
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 from torch.optim.lr_scheduler import _LRScheduler
 
+# helpers
 
 def grad_nan_to_num_(input: nn.Module|list[torch.Tensor], nan: float=0.0, posinf: float=1e5, neginf: float=1e-5) -> None:
     """et nan gardients to a number.
@@ -39,6 +40,67 @@ def grad_nan_to_num_(input: nn.Module|list[torch.Tensor], nan: float=0.0, posinf
     #         param.grad = grad.reshape(param.shape)
 
 
+def _assert_scaler(maybe_scaler: None|GradScaler):
+    assert maybe_scaler is None or isinstance(maybe_scaler, GradScaler), f'"scaler" must be None or an instance of GradScaler.'
+
+def _assert_require_module(maybe_module: None|nn.Module):
+    assert maybe_module is not None, f'"module" is required for "clip_grad_norm" or "grad_nan_to_num" option.'
+
+
+def simple_optimizer_step(
+    loss: torch.Tensor, optimizer: optim.Optimizer, scaler: GradScaler, scheduler: _LRScheduler=None, module: nn.Module=None,
+    *,
+    zero_grad: bool=True, set_to_none: bool=True,
+    clip_grad_norm: bool=False, max_norm: float=1.0, grad_nan_to_num: bool=False,
+    update_scaler: bool=False
+) -> None:
+    """optimization step which supports gradient scaling for AMP.
+    This implementation requires to use the GradScaler.enabled attribute for switching
+    whether to use gradient scaling/unscaling.
+
+
+    Args:
+        loss (torch.Tensor): loss to backpropagate.
+        optimizer (optim.Optimizer): optimizer.
+        scaler (GradScaler): Optional GradScaler object. whether to use gradient scaling
+            should be configured via "enabled" option in GradScaler.
+        scheduler (_LRScheduler, optional): learning rate scheduler. should only be specified when calling .step()
+            on every batch. Default: None.
+        module (nn.Module, optional): The module to optimize. Used for gradient clipping. Default: None.
+        zero_grad (bool, optional): Call .zero_grad() on optimizer before calling .backward() on loss. Default: True.
+        set_to_none (bool, optional): Set None to .grad instead of setting them to 0. Default: True.
+        clip_grad_norm (bool, optional): Clip gradient norm. Default: False.
+        max_norm (float, optional): Maximum norm used when clipping gradients. Default: 1.0.
+        grad_nan_to_num (bool, optional): Replace nan gradient: a number. Default: False.
+        update_scaler (bool, optional): Update the scaler if not None. Default: False.
+    """
+    if clip_grad_norm or grad_nan_to_num:
+        _assert_require_module(module)
+
+    if zero_grad:
+        optimizer.zero_grad(set_to_none=set_to_none)
+
+    scaler.scale(loss).backward()
+
+    if clip_grad_norm or grad_nan_to_num:
+        scaler.unscale_(optimizer)
+        if grad_nan_to_num:
+            grad_nan_to_num_(module)
+        if clip_grad_norm:
+            if isinstance(module, FSDP):
+                module.clip_grad_norm_(max_norm=max_norm)
+            else:
+                torch.clip_grad_norm(module.parameters(), max_norm=max_norm)
+
+    scaler.step(optimizer)
+
+    if update_scaler:
+        scaler.update()
+
+    if scheduler is not None:
+        scheduler.step()
+
+
 def optimizer_step(
     loss: torch.Tensor, optimizer: optim.Optimizer, scaler: GradScaler=None, scheduler: _LRScheduler=None, module: nn.Module=None,
     *,
@@ -47,6 +109,9 @@ def optimizer_step(
     update_scaler: bool=False
 ) -> None:
     """optimization step which supports gradient scaling for AMP.
+    This is a more general implementation compared to simple_optimizer_step,
+    which does not require a GradScaler object.
+
 
     Args:
         loss (torch.Tensor): loss to backpropagate.
@@ -55,6 +120,7 @@ def optimizer_step(
             If specified, uses it to scale the loss and call .step(). Default: None.
         scheduler (_LRScheduler, optional): learning rate scheduler. should only be specified when calling .step()
             on every batch. Default: None.
+        module (nn.Module, optional): The module to optimize. Used for gradient clipping. Default: None.
         zero_grad (bool, optional): Call .zero_grad() on optimizer before calling .backward() on loss. Default: True.
         set_to_none (bool, optional): Set None to .grad instead of setting them to 0. Default: True.
         clip_grad_norm (bool, optional): Clip gradient norm. Default: False.
@@ -62,9 +128,9 @@ def optimizer_step(
         grad_nan_to_num (bool, optional): Replace nan gradient: a number. Default: False.
         update_scaler (bool, optional): Update the scaler if not None. Default: False.
     """
-    assert scaler is None or isinstance(scaler, GradScaler)
+    _assert_scaler(scaler)
     if clip_grad_norm or grad_nan_to_num:
-        assert module is not None, f'"clip_grad_norm" or "grad_nan_to_num" option requires module argument.'
+        _assert_require_module(module)
 
     if zero_grad:
         optimizer.zero_grad(set_to_none=set_to_none)
@@ -103,8 +169,131 @@ def optimizer_step(
         scheduler.step()
 
 
+class simple_optimizer_step_with_gradient_accumulation:
+    """"torchops.optimizer_step" supporting gradient accumulation.
+    This implementation requires to use the GradScaler.enabled attribute for switching
+    whether to use gradient scaling/unscaling.
+
+    Args:
+        gradient_accumulation_steps (int): number of gradient accumulation steps.
+        num_iters_per_epoch (int): number of iterations per epoch. used to adjust the
+            accumulation steps of the last few batches.
+
+    Examples:
+        >>> optimizer_step = optimizer_step_with_gradient_accumulation(grad_steps, total_iters)
+        >>> # then "optimizer_step" can be used as same as "torchops.optimizer_step"
+    """
+
+    def __init__(self, gradient_accumulation_steps: int, num_iters_per_epoch: int, no_sync_context: Callable=None) -> None:
+        self.gradient_accumulation_steps = gradient_accumulation_steps
+        self.num_iters_per_epoch = num_iters_per_epoch
+
+        # calc last accumulation steps and where it starts.
+        # if gradient accumulation steps and total iterations are 4 and 13 respectively,
+        # last accumulation steps and where it starts would be 1 and 12 respectively.
+        quotient, remainder = divmod(num_iters_per_epoch, gradient_accumulation_steps)
+        self.last_gradient_accumulation_steps = remainder
+        self.last_accumulation_from = quotient * gradient_accumulation_steps
+
+        # counters
+        self.accumulation_count = 0
+        self.total_step_count = 0
+
+        self.no_sync = no_sync_context if no_sync_context is not None else nullcontext
+
+    def _reset(self):
+        """reset counters"""
+        self.accumulation_count = 0
+        self.total_step_count = self.total_step_count % self.num_iters_per_epoch
+
+
+    def _step(self) -> tuple[bool, float]:
+        """one step
+
+        Returns:
+            bool: whether to call .step() on optimizer or not.
+            float: value to scale the loss. generally equal to gradient accumulation steps.
+        """
+        self.accumulation_count += 1
+        self.total_step_count += 1
+
+        gradient_accumulation_steps = self.gradient_accumulation_steps
+        if self.total_step_count > self.last_accumulation_from:
+            gradient_accumulation_steps = self.last_gradient_accumulation_steps
+
+        if self.accumulation_count == gradient_accumulation_steps:
+            self._reset()
+            return True, 1.0 / gradient_accumulation_steps
+        return False, 1.0 / gradient_accumulation_steps
+
+
+    def __call__(self,
+        loss: torch.Tensor, optimizer: optim.Optimizer, scaler: GradScaler, scheduler: _LRScheduler=None, module: nn.Module=None,
+        *,
+        zero_grad: bool=True, set_to_none: bool=True,
+        clip_grad_norm: bool=False, max_norm: float=1.0, grad_nan_to_num: bool=False,
+        update_scaler: bool=False
+    ) -> None:
+        """optimization step which supports gradient scaling for AMP.
+
+        Args:
+            loss (torch.Tensor): loss to backpropagate.
+            optimizer (optim.Optimizer): optimizer.
+            scaler (GradScaler): Optional GradScaler object.
+                If specified, uses it to scale the loss and call .step(). Default: None.
+            scheduler (_LRScheduler, optional): learning rate scheduler. should only be specified when calling .step()
+                on every batch. Default: None.
+            module (nn.Module, optional): The module to optimize. Used for gradient clipping. Default: None.
+            zero_grad (bool, optional): Call .zero_grad() on optimizer before calling .backward() on loss. Default: True.
+            set_to_none (bool, optional): Set None to .grad instead of setting them to 0. Default: True.
+            clip_grad_norm (bool, optional): Clip gradient norm. Default: False.
+            max_norm (float, optional): Maximum norm used when clipping gradients. Default: 1.0.
+            grad_nan_to_num (bool, optional): Replace nan gradient: a number. Default: False.
+            update_scaler (bool, optional): Update the scaler if not None. Default: False.
+        """
+        if clip_grad_norm or grad_nan_to_num:
+            _assert_require_module(module)
+
+        should_call_step, loss_scale = self._step()
+        loss = loss * loss_scale
+
+        no_sync = nullcontext if should_call_step else self.no_sync
+
+        with no_sync():
+            if scaler is not None:
+                scaler.scale(loss).backward()
+            else:
+                loss.backward()
+
+        if not should_call_step:
+            return
+
+        if clip_grad_norm or grad_nan_to_num:
+            scaler.unscale_(optimizer)
+            if grad_nan_to_num:
+                grad_nan_to_num_(module.parameters())
+            if clip_grad_norm:
+                if isinstance(module, FSDP):
+                    module.clip_grad_norm_(max_norm)
+                else:
+                    torch.nn.utils.clip_grad_norm_(module.parameters(), max_norm)
+
+        scaler.step(optimizer)
+
+        if update_scaler:
+            scaler.update()
+
+        if scheduler is not None:
+            scheduler.step()
+
+        if zero_grad:
+            optimizer.zero_grad(set_to_none=set_to_none)
+
+
 class optimizer_step_with_gradient_accumulation:
     """"torchops.optimizer_step" supporting gradient accumulation.
+    This is a more general implementation compared to simple_optimizer_step_with_gradient_accumulation,
+    which does not always require a GradScaler object.
 
     Args:
         gradient_accumulation_steps (int): number of gradient accumulation steps.
