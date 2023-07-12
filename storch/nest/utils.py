5622ded665012808e0ef1ea5c6b4acee732ec150
Tomoya Sawada <stomoya0110@gmail.com>
2023-07-12 19:09:21 +0900

    🎉 NeST init commit

---
+++ storch/nest/utils.py
@@ -0,0 +1,17 @@
+
+from functools import wraps
+
+
+def _noops(*args, **kwargs):
+    return
+
+
+def _assert_initialized(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        assert self.initialized, (
+            f'NeST is not initialized. To call {func.__name__} you must call `NeST.initialize_training` '
+            'before calling this function.'
+        )
+        return func(self, *args, **kwargs)
+    return wrapper
