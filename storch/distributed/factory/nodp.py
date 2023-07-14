c5f4b48e082d080348de4c90930ebec7c4dec062
Tomoya Sawada <stomoya0110@gmail.com>
2023-07-14 22:32:53 +0900

    📚 docs

--- storch/distributed/factory/nodp.py
+++ storch/distributed/factory/nodp.py
@@ -6,6 +6,7 @@ from storch.distributed.factory._base import ParallelFactoryBase
 
 
 class NoParallelFactory(ParallelFactoryBase):
+    """Dummy class for no distributed training."""
 
     def wrap_module(self, module: nn.Module, **kwargs):
         self._wrapped_module = module
