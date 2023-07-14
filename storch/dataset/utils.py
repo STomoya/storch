d87e7e27177994f65f6c7a8a31b6d082a102fd68
Tomoya Sawada <stomoya0110@gmail.com>
2023-07-14 21:25:19 +0900

    🔨 replace to `torch`

--- storch/dataset/utils.py
+++ storch/dataset/utils.py
@@ -10,6 +10,7 @@ import storch
 # from https://github.com/pytorch/vision/blob/6512146e447b69cc9fb379eb05e447a17d7f6d1c/torchvision/datasets/folder.py#L242
 IMG_EXTENSIONS = {".jpg", ".jpeg", ".png", ".ppm", ".bmp", ".pgm", ".tif", ".tiff", ".webp"}
 
+
 def is_image_file(path: str) -> bool:
     """Returns whether if the path is a PIL.Image.Image openable file
 
@@ -21,6 +22,7 @@ def is_image_file(path: str) -> bool:
     """
     ext = set([os.path.splitext(os.path.basename(path))[-1].lower()])
     return ext.issubset(IMG_EXTENSIONS)
+
 
 def get_loader_kwargs() -> storch.EasyDict:
     """loader keyword arguments.
@@ -38,12 +40,12 @@ def get_loader_kwargs() -> storch.EasyDict:
     loader_kwargs.generator      = None
     return loader_kwargs
 
-def is_tensor(data):
-    return isinstance(data, torch.Tensor)
+
 def to(data, device):
     return data.to(device)
 
+
 def device_placement_collate_fn(batch, device):
     batch = default_collate(batch)
-    batch = storch.recursive_apply(partial(to, device=device), batch, is_tensor)
+    batch = storch.recursive_apply(partial(to, device=device), batch, torch.is_tensor)
     return batch
